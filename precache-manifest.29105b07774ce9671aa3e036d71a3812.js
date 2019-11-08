10423195f023549c5f2b5312e6a0f02b0c3da1ab
Pavlo Dudar <dudar.pavloGmail.com>
2019-11-08 14:14:49 +0200

    Updates

---
+++ precache-manifest.29105b07774ce9671aa3e036d71a3812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cacc1ee6ce06cb05cda760b4a0af5a8",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "27bc1fe359def1923425",
+    "url": "/myapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "07f9361d50ce0554af0d",
+    "url": "/myapp/static/js/2.b5985af0.chunk.js"
+  },
+  {
+    "revision": "27bc1fe359def1923425",
+    "url": "/myapp/static/js/main.9845acc9.chunk.js"
+  },
+  {
+    "revision": "27343e7b7cdb70592f08",
+    "url": "/myapp/static/js/runtime-main.f1bd5847.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myapp/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.90c13e2b61d9f576a5bd420708b11e1b.js"
+  "/myapp/precache-manifest.29105b07774ce9671aa3e036d71a3812.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
