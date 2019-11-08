88364d377aa503757234a9a224fe5266c597f073
Pavlo Dudar <dudar.pavloGmail.com>
2019-11-08 14:09:30 +0200

    Updates

---
+++ precache-manifest.90c13e2b61d9f576a5bd420708b11e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27c2210af0919fa405356e6054a6ea8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c59fc86e5e6d65267afd",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "07f9361d50ce0554af0d",
+    "url": "/my-app/static/js/2.b5985af0.chunk.js"
+  },
+  {
+    "revision": "c59fc86e5e6d65267afd",
+    "url": "/my-app/static/js/main.d3c925e7.chunk.js"
+  },
+  {
+    "revision": "b4d8a98f0b4f22fd2330",
+    "url": "/my-app/static/js/runtime-main.d5440eda.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
